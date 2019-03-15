c61594dde16c524b4fb8b707ddc8e7d30d270fcb
Pranav Udupa S <pranavudupa94@gmail.com>
2019-03-15 16:50:41 -0700

    Add read/write binary tree from file

--- lab9/BinaryTree.cpp
+++ lab9/BinaryTree.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 using namespace std;
+
+void tokenzie(vector<string> &levelNodes, string levelString)
+{
+    stringstream ss;
+    ss << levelString;
+    while (ss.good())
+    {
+        string t;
+        getline(ss, t, ',');
+        levelNodes.push_back(t);
+    }
+}
+
+string getFileName()
+{
+    cout << "Enter file name: ";
+    string ip;
+    cin >> ip;
+    return ip;
+}
 
 class TreeNode
 {
-  private:
   public:
     int data;
     TreeNode *left;
     TreeNode *right;
     TreeNode(int data);
-    ~TreeNode();
 };
 
 TreeNode::TreeNode(int data)
@@ -19,40 +41,262 @@ TreeNode::TreeNode(int data)
     this->right = nullptr;
 }
 
-TreeNode::~TreeNode()
-{
-    if(left != nullptr)
-        (*left).~TreeNode();
-    if(right != nullptr)
-        (*right).~TreeNode();
-}
-
 class BinaryTree
 {
-  private:
-    /* data */
   public:
     TreeNode *root;
     BinaryTree();
     ~BinaryTree();
+    void readTree();
+    void writeTree();
+    void loadTree(vector<string> levels);
+    void inorder();
+    void preorder();
+    void postorder();
+    void levelorder();
+    void deleteTree();
+    void inorder(TreeNode *root);
+    void preorder(TreeNode *root);
+    void postorder(TreeNode *root);
+    void levelorder(vector<vector<TreeNode *>> &levels, TreeNode *root, int level);
+    void deleteTree(TreeNode *root);
 };
 
 BinaryTree::BinaryTree()
 {
-    this->root = new TreeNode(4);
-    this->root->left = new TreeNode(2);
-    this->root->right = new TreeNode(5);
-    this->root->left->left = new TreeNode(4);
-    this->root->right->left = new TreeNode(6);
-    cout << this->root->data << " " << this->root->left->data << " " << this->root->right->data;
+    root = nullptr;
 }
 
 BinaryTree::~BinaryTree()
 {
+    deleteTree(root);
+}
+
+void BinaryTree::levelorder()
+{
+    vector<vector<TreeNode *>> levels;
+    levelorder(levels, root, 0);
+    levels.pop_back();
+    for (vector<TreeNode *> &level : levels)
+    {
+        for (TreeNode *&node : level)
+            if(node != nullptr)
+                cout << node->data << " ";
+        cout << endl;
+    }
+}
+
+void BinaryTree::levelorder(vector<vector<TreeNode *>> &levels, TreeNode *node, int level)
+{
+    if ((int)levels.size() == level)
+        levels.push_back(vector<TreeNode *>());
+    if (node == nullptr)
+    {
+        levels[level].push_back(nullptr);
+        return;
+    }
+    levels[level].push_back(node);
+    levelorder(levels, node->left, level + 1);
+    levelorder(levels, node->right, level + 1);
+}
+
+void BinaryTree::loadTree(vector<string> levels)
+{
+    if (levels.size() == 0)
+    {
+        root = nullptr;
+        return;
+    }
+    root = new TreeNode(stoi(levels[0]));
+    vector<vector<TreeNode *>> nodes;
+    nodes.push_back(vector<TreeNode *>());
+    nodes[0].push_back(root);
+    for (int level = 1; level < (int)levels.size(); level++)
+    {
+        nodes.push_back(vector<TreeNode *>());
+        int j = 0;
+        vector<string> levelNodes;
+        tokenzie(levelNodes, levels[level]);
+        for (int i = 0; i < (int)nodes[level - 1].size(); i++)
+        {
+            TreeNode *node = nodes[level - 1][i];
+            if (node == nullptr)
+                continue;
+            TreeNode *leftChild = nullptr;
+            TreeNode *rightChild = nullptr;
+            if (j < (int)levelNodes.size() && levelNodes[j] != "")
+                leftChild = new TreeNode(stoi(levelNodes[j]));
+            j++;
+
+            if (j < (int)levelNodes.size() && levelNodes[j] != "")
+                rightChild = new TreeNode(stoi(levelNodes[j]));
+            j++;
+
+            node->left = leftChild;
+            node->right = rightChild;
+            nodes[i + 1].push_back(leftChild);
+            nodes[i + 1].push_back(rightChild);
+        }
+    }
+}
+
+void readLevels(vector<string> &levels)
+{
+    string level;
+    ifstream inputFile(getFileName());
+    if (!inputFile.good())
+        cout << "File does not exist" << endl;
+    else
+        while (getline(inputFile, level))
+            levels.push_back(level);
+    inputFile.close();
+}
+
+void BinaryTree::readTree()
+{
+    string level;
+    vector<string> levels;
+    readLevels(levels);
+    loadTree(levels);
+}
+
+void BinaryTree::writeTree()
+{
+    string op = getFileName();
+    vector<vector<TreeNode *>> levels;
+    levelorder(levels, root, 0);
+    ofstream ofile;
+    ofile.open(op);
+    for (vector<TreeNode *> &level : levels)
+    {
+        for (TreeNode *&node : level)
+        {
+            if (node != nullptr)
+                ofile << node->data << ",";
+            else
+                ofile << ",";
+        }
+
+        ofile << endl;
+    }
+    ofile.close();
+}
+
+void BinaryTree::inorder()
+{
+    cout << "Inorder: ";
+    inorder(root);
+}
+
+void BinaryTree::preorder()
+{
+    cout << "Pre-order: ";
+    preorder(root);
+}
+
+void BinaryTree::postorder()
+{
+    cout << "Post-order: ";
+    postorder(root);
+}
+
+void BinaryTree::deleteTree()
+{
+    deleteTree(root);
+}
+
+void BinaryTree::inorder(TreeNode *root)
+{
+    if (root != nullptr)
+    {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+void BinaryTree::preorder(TreeNode *root)
+{
+    if (root != nullptr)
+    {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void BinaryTree::postorder(TreeNode *root)
+{
+    if (root != nullptr)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+void BinaryTree::deleteTree(TreeNode *root)
+{
+    if (root != nullptr)
+    {
+        deleteTree(root->left);
+        deleteTree(root->right);
+        delete root;
+    }
+}
+
+void printOptions()
+{
+    cout << endl
+         << endl;
+    cout << "Enter command:" << endl;
+    cout << "1: Read tree" << endl;
+    cout << "2: Write tree" << endl;
+    cout << "3: Inorder" << endl;
+    cout << "4: Pre-order" << endl;
+    cout << "5: Post-order" << endl;
+    cout << "6: Level order" << endl;
+}
+
+void parseCmd(BinaryTree &tree)
+{
+    int input;
+    cin >> input;
+    cout << endl;
+    switch (input)
+    {
+    case 1:
+        tree.readTree();
+        break;
+    case 2:
+        tree.writeTree();
+        break;
+    case 3:
+        tree.inorder();
+        break;
+    case 4:
+        tree.preorder();
+        break;
+    case 5:
+        tree.postorder();
+        break;
+    case 6:
+        tree.levelorder();
+        break;
+
+    default:
+        break;
+    }
 }
 
 int main()
 {
     BinaryTree tree = BinaryTree();
+    while (!cin.eof())
+    {
+        printOptions();
+        parseCmd(tree);
+    }
+    cout << endl;
     return 0;
 }
